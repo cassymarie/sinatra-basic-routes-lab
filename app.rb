0c3098807711bee9d0f91a408efc86ca463a5133
cassymarie <cassy2205@gmail.com>
2020-11-24 12:27:19 -0700

    commit tree ed9ab278ef78aa07c7f082b82e0f69cf91f140cc

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+     get '/favorite-song' do
+          "My favorite song is __"
+     end
+     get '/name' do
+          "My name is Cassy"
+     end
+     get '/hometown' do
+          "My hometown is __"
+     end
 end
